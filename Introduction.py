402735f3ab658565b66c432ce12a12db3fa83a7b
Khande Sai Prakash <120784159+khandesaiprakash@users.noreply.github.com>
2024-05-19 16:30:53 +0530

    Introduction & Features

---
+++ Introduction.py
@@ -0,0 +1,34 @@
+"""Python Programming
+
+Introduction: -
+Python is a General-Purpose programming language
+Python was developed by GUIDO VAN ROSSAM in 1989 and it is available in 1991.
+Using Python developed: -
+Web application 
+Mobile application
+Desktop apps
+Testing
+Data Science and AI
+IOT
+
+Features of the python programming
+Simple and Easy
+Open Source: - 
+ It is available in free of cost.
+Platform Independent: - 
+Python doesn’t depend what platform to execute the programs. 
+Dynamically typed: - 
+Python can’t define any data types, directly we can write the data.
+Procedure Oriented & Object Oriented: - 
+Python can be works using functions like C and also it works classes and objects like JAVA. 
+Interpreted: -  
+Python code directly run the programs without compilation
+Extensible: - 
+Python can extend the support to other programming language like C, JAVA.
+Embedded
+Huge Library: -
+Python can be use huge amount of tools/ libraries."""
+
+
+
+ 
