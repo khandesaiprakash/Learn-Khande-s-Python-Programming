2c8726f68abe789658ceadc64e3f42bd1d41749e
Khande Sai Prakash <120784159+khandesaiprakash@users.noreply.github.com>
2024-05-19 17:01:11 +0530

    Fundamentals.py

---
+++ Fundamentals.py
@@ -0,0 +1,33 @@
+""" Fundamentals:
+A)Identifiers
+B)KeyWords
+C)Comments
+D)Operators"""
+
+# Identifiers:-
+# A name in python programming is called Identifiers. 
+#It can be Variables names, class names & function names
+
+#Rules of the Identifiers:-
+#-Only alpabets, digits, underscore symbols are allowed to use(a=1, a1=5, a_=5). 
+#-Special Symboles are not allowed.($=20)
+#Identifiers should not start with degit. 
+#Identifiers are case sensitive. 
+#we can't use 'keywords' as identifier. 
+
+#Keywords: -
+# These are the reserved words to represent some meaning or functionality. 
+
+#Check the Keywors in your version
+import keyword
+
+# Get the list of keywords
+keywords = keyword.kwlist
+
+# Print the list of keywords
+print(keywords)
+
+#c) Comments: -
+#write the code, the code will be understand other people. 
+#Single line comments are represent (#Hello) and 
+# Multiple line comments are represent (""" Hello """)
